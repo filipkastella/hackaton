189a5dc726fca5aafc0753695e680bee6be9ed34
Filip Kastella <kastellafilip@gmail.com>
2025-11-08 20:21:40 +0100

    fix: add missing import for updatePosDTO in GroupService

--- backend/src/main/java/com/backend/hackaton/services/GroupService.java
+++ backend/src/main/java/com/backend/hackaton/services/GroupService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.backend.hackaton.dto.GroupDTO;
+import com.backend.hackaton.dto.updatePosDTO;
 import com.backend.hackaton.models.Member;
 import com.backend.hackaton.repositories.GroupRepository;
 
