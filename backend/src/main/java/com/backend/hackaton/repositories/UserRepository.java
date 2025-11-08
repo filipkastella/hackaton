269d928f0b0feccccf97cf7a21bc82ee14fbcfc9
Filip Kastella <kastellafilip@gmail.com>
2025-11-08 13:16:53 +0100

    refactor: remove unnecessary import statement in UserRepository

--- backend/src/main/java/com/backend/hackaton/repositories/UserRepository.java
+++ backend/src/main/java/com/backend/hackaton/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.backend.hackaton.repositories;
 
 import java.util.Optional;
-
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
