d564ea4a9bff3e16d8bc6ce297ca604d0b4fae0f
Filip Kastella <kastellafilip@gmail.com>
2025-11-08 09:30:21 +0100

    modified gitignore and pom.xml, added User model class

---
+++ backend/src/main/java/com/backend/hackaton/models/User.java
@@ -0,0 +1,14 @@
+package com.backend.hackaton.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    
+}
